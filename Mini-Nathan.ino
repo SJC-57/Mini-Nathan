a0749274bff5b166d4a4fef1664991ef2fdd986b
SJC-57 <sajcove@gmail.com>
2026-04-28 20:54:14 -0500

    Update Mini-Nathan.ino

--- Mini-Nathan.ino
+++ Mini-Nathan.ino
@@ -1,8 +1,39 @@
+#include Servo.h
+//primera librería, después agrego más
+
+Servo cabeza;
+int buzzer = 8;
+int led = 7 
+  
 void setup() {
+  Serial.begin(9600)
+  cabeza.attach(9);
+  pinMode(led, OUTPUT);
+  
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  cabeza.write(0);
+  delay(1000);
+  cabeza.write(90);
+  delay(1000);
+  //prueba del servo
+
+  tone(buzzer, 440)
+  delay(500)
+  noTone(buzzer);
+  delay(500);
+  //prueba del buzzer
+
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(500);
+
+
+
 
 }
