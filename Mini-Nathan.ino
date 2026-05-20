76b10a37fb69511e0e705d93f23b4256234ac2f3
SJC-57 <sajcove@gmail.com>
2026-05-19 23:30:34 -0500

    Updates

--- Mini-Nathan.ino
+++ Mini-Nathan.ino
@@ -1,36 +1,40 @@
-#include Servo.h
-#include 
+#include <Servo.h>
+
 
 Servo cabeza;
+int i = 0;
 int buzzer = 8;
-int led = 7 
-int a = 
-int b = 
-int c = 
-int d = 
-int e = 
-int f = 
-int g = 
+int led = 7;
+int a = 3;
+int b = 4;
+int c = 5;
+int d = 6;
+int e = 9;
+int f = 10;
+int g = 11;
   
 void setup() {
-  Serial.begin(9600)
+  Serial.begin(9600);
   cabeza.attach(9);
   pinMode(led, OUTPUT);
-  
-
 
 }
 
 void loop() {
-  
-  cabeza.write(0);
-  delay(1000);
-  cabeza.write(90);
-  delay(1000);
+
+  for (i = 0; i <= 180; i ++){
+    cabeza.write(i);
+    delay(20);
+  }
+
+  for (i = 180;  i >= 0; i --){
+    cabeza.write(i);
+    delay(20);
+  }
   //prueba del servo
 
-  tone(buzzer, 440)
-  delay(500)
+  tone(buzzer, 440);
+  delay(500);
   noTone(buzzer);
   delay(500);
   //prueba del buzzer
@@ -39,6 +43,9 @@ void loop() {
   delay(1000);
   digitalWrite(led, LOW);
   delay(500);
+  //prueba del led
+
+
 
 
 
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Este es el primer proyecto de hardware que he hecho.
 Robot que sirve tanto como reloj digital y reproductor de música básico.
 Consiste de servos que hacen mover una cabeza, un buzzer que reproduce musica, un modulo de tiempo, un display para mostrar la hora y dos leds.
 
+Materiales: 
+
+Arduino Uno 
+TM1637 4-Digit Display
+servos 
+buzzer pasivo
+LEDs
+DS3231 RTC Module 
